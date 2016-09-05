648e44420a9a29746c12fc33ccb2d5f40103f039
Abu Ali Muhammad Sharjeel <sheryever@hotmail.com>
2016-09-05 07:09:20 +0300

    Add project files.

---
+++ JtableAttributes/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JtableAttributes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Orders()
+        {
+            return  Json(new {Result = "OK", Records = GetOrders()});
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public IList<Order> GetOrders()
+        {
+            return new [] {new Order
+            {
+                OrderId = 1, ShipName = "Ship 1", ShipCountry = "Pakistan", OrderDate = DateTime.Now.AddDays(1), Shipped = true 
+            }, new Order
+            {
+                OrderId = 2, ShipName = "Ship 2", ShipCountry = "Pakistan", OrderDate = DateTime.Now.AddDays(3), Shipped = true
+            }, new Order
+            {
+                OrderId = 3, ShipName = "Ship 3", ShipCountry = "Pakistan", OrderDate = DateTime.Now.AddDays(2), Shipped = false 
+            
+            }, new Order
+            {
+                OrderId = 4, ShipName = "Ship 4", ShipCountry = "Pakistan", OrderDate = DateTime.Now.AddDays(5), Shipped = false 
+            
+            }, new Order
+            {
+                OrderId = 5, ShipName = "Ship 5", ShipCountry = "Pakistan", OrderDate = DateTime.Now.AddDays(3), Shipped = true 
+            
+            } };
+        }
+
+
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool Shipped { get; set; }
+    }
+}
---
+++ JtableAttributes/Scripts/jtable/extensions/jquery.jtable.buildFromAttributes.js
@@ -0,0 +1,115 @@
+﻿(function() {
+	var getSetting = function ($element, attribute, defaultVal, fieldSettings, valFoundCallback, defaultValueCallBack) {
+	    var val = $element.data('jt-' + attribute);
+	    if (val !== undefined && val !== null) {
+		    if (valFoundCallback) {
+		    	valFoundCallback($element, val, fieldSettings);
+		    }
+		    return val;
+	    } else {
+		    if (defaultValueCallBack) {
+		    	defaultValueCallBack($element, val, fieldSettings);
+		    }
+		    return defaultVal;
+	    }
+    }
+
+    $.hik.jtable.loadFromAttributes = function (configClass) {
+	    if (configClass === undefined || configClass === null) {
+	        configClass = '.jt-config';
+	    }
+
+	    $.hik.jtable.initFromAttributes(configClass).jtable('load');
+
+        //$(configClass)
+		    //.each(function (idx, obj) {
+		    //	//console.log(obj);
+		    //	$(obj).jtable('load');
+		    //});
+
+    }
+
+    $.hik.jtable.buildConfigFromAttributes = function(configElementId) {
+    	var $config = $('#' + configElementId);
+
+    	var jtableConfig = {
+    		title: getSetting($config, 'title', ""),
+    		actions: {
+    			listAction: getSetting($config, 'list-action', "")
+    		},
+    		fields: {
+
+    		}
+    	}
+    	var keyFound = false;
+    	var firstField = null;
+
+
+    	$config.find("th")
+			.each(function (idx, obj) {
+
+				var $th = $(obj);
+				var fieldName = getSetting($th, 'column', $th.text().replace(' ', ''));
+				var fieldTitle = getSetting($th, 'title', $th.text());
+				if (idx === 0) {
+					firstField = fieldName;
+				}
+
+				var fieldConfig = {
+					title: fieldTitle,
+					width: getSetting($th, 'width', '10%'),
+					list: getSetting($th, 'list', true)
+				}
+				fieldConfig.key = getSetting($th, 'key', false, fieldConfig,
+	                function(value) {
+	                    keyFound = true;
+	                });
+
+
+	            fieldConfig.type = getSetting($th, 'type', 'string', fieldConfig,
+	                function($element, val, fieldSettings) {
+	                    if (val === "checkbox") {
+	                        console.log(this);
+	                        fieldSettings.values = {
+	                            'false': getSetting($element, 'false', 'False'),
+	                            'true': getSetting($element, 'true', 'True')
+	                        }
+	                    }
+	                });
+
+				jtableConfig.fields[fieldName] = fieldConfig;
+			});
+
+    	if (!keyFound) {
+    		jtableConfig.fields[firstField].key = true;
+    	}
+
+    	//$config.html('');
+    	console.log(jtableConfig);
+        return jtableConfig;
+    }
+
+    $.hik.jtable.initFromAttributes = function (configClass) {
+	    if (configClass === undefined || configClass === null) {
+		    configClass = '.jt-config';
+	    }
+	    $(configClass)
+		    .each(function (idx, obj) {
+		    	//console.log(obj);
+		    	var jtableConfig = $.hik.jtable.buildConfigFromAttributes(obj.id);
+		    	var $config = $('#' + obj.id);
+
+		    	$config.html('');
+
+			    var jtableId = $config.attr('id');
+			    var jtableContainer = '<div  id="' + jtableId + '" class="' + configClass.replace('.', '') + '"></div>';
+
+			    $('#' + jtableId)
+				    .replaceWith(jtableContainer);
+
+			    $('#' + jtableId).jtable(jtableConfig);
+
+		    });
+	    return $(configClass);
+    }
+})();
